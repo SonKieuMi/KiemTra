072d7dbb3befd009487166bb8a51220d3c3e5459
KieuMi <kieumixd123@gmail.com>
2022-03-11 14:26:59 +0700

    commit matkhau

--- KiemTra/KiemTra/Form1.Designer.cs
+++ KiemTra/KiemTra/Form1.Designer.cs
@@ -31,32 +31,54 @@ namespace KiemTra
         {
             this.label1 = new System.Windows.Forms.Label();
             this.txtUserName = new System.Windows.Forms.TextBox();
+            this.lab = new System.Windows.Forms.Label();
+            this.txtmatkhau = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(96, 49);
+            this.label1.Location = new System.Drawing.Point(192, 95);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.Size = new System.Drawing.Size(145, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "Username";
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(274, 49);
+            this.txtUserName.Location = new System.Drawing.Point(548, 95);
+            this.txtUserName.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(100, 22);
+            this.txtUserName.Size = new System.Drawing.Size(196, 38);
             this.txtUserName.TabIndex = 1;
+            // 
+            // lab
+            // 
+            this.lab.AutoSize = true;
+            this.lab.Location = new System.Drawing.Point(192, 200);
+            this.lab.Name = "lab";
+            this.lab.Size = new System.Drawing.Size(131, 32);
+            this.lab.TabIndex = 2;
+            this.lab.Text = "Mật khẩu";
+            // 
+            // txtmatkhau
+            // 
+            this.txtmatkhau.Location = new System.Drawing.Point(548, 215);
+            this.txtmatkhau.Name = "txtmatkhau";
+            this.txtmatkhau.Size = new System.Drawing.Size(196, 38);
+            this.txtmatkhau.TabIndex = 3;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(763, 232);
+            this.ClientSize = new System.Drawing.Size(1526, 450);
+            this.Controls.Add(this.txtmatkhau);
+            this.Controls.Add(this.lab);
             this.Controls.Add(this.txtUserName);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -68,6 +90,8 @@ namespace KiemTra
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label lab;
+        private System.Windows.Forms.TextBox txtmatkhau;
     }
 }
 
