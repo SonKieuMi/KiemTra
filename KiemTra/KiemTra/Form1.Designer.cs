0117bea27772caf089ac7770c5546bd9b85369df
Hoang Vy <vyh6650@gmail.com>
2022-03-11 14:44:59 +0700

    commit đăng nhập

--- KiemTra/KiemTra/Form1.Designer.cs
+++ KiemTra/KiemTra/Form1.Designer.cs
@@ -39,61 +39,58 @@ namespace KiemTra
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(192, 95);
-            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Location = new System.Drawing.Point(96, 49);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(197, 32);
+            this.label1.Size = new System.Drawing.Size(100, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "tên đăng nhập";
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(548, 95);
-            this.txtUserName.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtUserName.Location = new System.Drawing.Point(274, 49);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(196, 38);
+            this.txtUserName.Size = new System.Drawing.Size(100, 22);
             this.txtUserName.TabIndex = 1;
             // 
             // lab
             // 
             this.lab.AutoSize = true;
-            this.lab.Location = new System.Drawing.Point(192, 200);
-            this.lab.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lab.Location = new System.Drawing.Point(96, 103);
+            this.lab.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lab.Name = "lab";
-            this.lab.Size = new System.Drawing.Size(131, 32);
+            this.lab.Size = new System.Drawing.Size(66, 17);
             this.lab.TabIndex = 2;
             this.lab.Text = "mật khẩu";
             this.lab.Click += new System.EventHandler(this.lab_Click);
             // 
             // txtpassword
             // 
-            this.txtpassword.Location = new System.Drawing.Point(548, 215);
-            this.txtpassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtpassword.Location = new System.Drawing.Point(274, 111);
+            this.txtpassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtpassword.Name = "txtpassword";
-            this.txtpassword.Size = new System.Drawing.Size(196, 38);
+            this.txtpassword.Size = new System.Drawing.Size(100, 22);
             this.txtpassword.TabIndex = 3;
             // 
             // btnlogin
             // 
-            this.btnlogin.Location = new System.Drawing.Point(548, 343);
-            this.btnlogin.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnlogin.Location = new System.Drawing.Point(274, 177);
             this.btnlogin.Name = "btnlogin";
-            this.btnlogin.Size = new System.Drawing.Size(150, 45);
+            this.btnlogin.Size = new System.Drawing.Size(116, 34);
             this.btnlogin.TabIndex = 4;
-            this.btnlogin.Text = "login";
+            this.btnlogin.Text = "đăng nhập";
             this.btnlogin.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1526, 450);
+            this.ClientSize = new System.Drawing.Size(763, 232);
             this.Controls.Add(this.btnlogin);
             this.Controls.Add(this.txtpassword);
             this.Controls.Add(this.lab);
             this.Controls.Add(this.txtUserName);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
